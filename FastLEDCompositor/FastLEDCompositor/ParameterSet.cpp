c0bf6cd3e927ac623e6b5d84cd5f50213da4efbe
unimatrix27 <sebastian.stuecker@gmail.com>
2015-11-23 10:24:32 +0100

    added missing file

---
+++ FastLEDCompositor/FastLEDCompositor/ParameterSet.cpp
@@ -0,0 +1,103 @@
+// 
+// 
+// 
+
+#include "ParameterSet.h"
+#include "disco.h"
+DEFINE_GRADIENT_PALETTE(GMT_ocean_gp) {
+	0, 0, 0, 0,
+		31, 0, 1, 1,
+		63, 0, 1, 4,
+		95, 0, 19, 42,
+		127, 0, 79, 138,
+		159, 15, 144, 112,
+		191, 91, 233, 89,
+		223, 155, 244, 158,
+		255, 242, 255, 255
+};
+ParameterSet::ParameterSet()
+{
+	effectType = ET_RAINBOW;
+	hue = 0;
+	saturation = 255;
+	brightness = 128;
+	vitality = 24;
+	speed = 0;
+	randomness = 0;
+	hueVariability = 0;
+	blurryness = 0;
+	currentPalette = RainbowColors_p;
+	targetPalette = RainbowColors_p;
+	paletteBlendType = LINEARBLEND;
+	paletteBlendStarttime = 0;
+	paletteBlendEndtime = 0;
+	clonecount = 0;
+	clonedistance = 50;
+	mirroraxes = 0;
+	channelSpeed = 0;
+	fadeTimeBase = TB_MILLIS;
+	fadeTime = 1000;
+	paletteBlendDuration = 4;
+	fadeType = FT_NOFADE;
+	blendType = BT_OVERLAY;
+	numLeds = NUM_LEDS;
+	startPos = 0;
+
+}
+ParameterSet::~ParameterSet() {
+
+}
+
+void ParameterSet::setTargetPalette(PaletteName myPalette, long duration) {
+	CRGBPalette16 blendedPalette;
+	switch (myPalette) {
+	default:
+	case PN_RAINBOW: targetPalette = RainbowColors_p; break;
+	case PN_RAINBOWSTRIPE: targetPalette = RainbowStripeColors_p; break;
+	case PN_FOREST: targetPalette = ForestColors_p; break;
+	case PN_LAVA: targetPalette = LavaColors_p; break;
+	case PN_CLOUD:  targetPalette = CloudColors_p; break;
+	case PN_OCEAN:  targetPalette = OceanColors_p; break;
+	case PN_PARTY:  targetPalette = PartyColors_p; break;
+	case PN_HEAT:  targetPalette = HeatColors_p; break;
+	case PN_CPT_OCEAN: targetPalette = GMT_ocean_gp; break;
+	case PN_AUTO_RANDOM:  makeRandom(&targetPalette); break;
+	}
+	if (duration == 0) {
+		currentPalette = targetPalette;
+		paletteBlendEndtime = paletteBlendStarttime = 0;
+	}
+	else {
+		if (paletteBlendEndtime > paletteBlendStarttime && millis() < paletteBlendEndtime) {
+			blend(currentPalette, targetPalette, blendedPalette, 16, (millis() - paletteBlendStarttime) / (paletteBlendEndtime - paletteBlendStarttime));
+			currentPalette = blendedPalette;
+		}
+		paletteBlendStarttime = millis();
+		paletteBlendEndtime = paletteBlendStarttime + duration;
+	}
+}
+
+void ParameterSet::makeRandom(CRGBPalette16* target) {
+	for (int i = 0; i < 16; i++) {
+		(*target)[i] =  CHSV(random8(), 255, 255);
+	}
+}
+
+struct CRGB ParameterSet::getColor(uint8_t index) {
+	struct CRGB result;
+
+	if (paletteBlendEndtime != paletteBlendStarttime && millis() >= paletteBlendEndtime) {
+		paletteBlendEndtime = paletteBlendStarttime;
+		currentPalette = targetPalette;
+	}
+	if (paletteBlendEndtime != paletteBlendStarttime) {
+		uint8_t passed = 255* (millis() - paletteBlendStarttime) / (paletteBlendEndtime - paletteBlendStarttime);
+		result = blend(ColorFromPalette(currentPalette, index, 255, this->paletteBlendType),
+			ColorFromPalette(targetPalette, index, 255, this->paletteBlendType),passed);
+		return result;
+	}
+	else {
+		result = ColorFromPalette(currentPalette, index,255, this->paletteBlendType);
+		return result;
+	}
+}
---
+++ FastLEDCompositor/FastLEDCompositor/ParameterSet.h
@@ -0,0 +1,68 @@
+// ParameterSet.h
+// A ParameterSet Object holds all configuration data for a channel and the effect played on it. All paramters to be influenced by MIDI or other sequencing means. 
+
+#ifndef _PARAMETERSET_h
+#define _PARAMETERSET_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "disco.h"
+
+// ALl pallettes used must be listed here to be accessible later. 
+enum PaletteName { PN_RAINBOW, PN_RAINBOWSTRIPE, PN_LAVA, PN_OCEAN, PN_CLOUD, PN_FOREST, PN_PARTY, PN_HEAT, PN_CPT_OCEAN, PN_AUTO_RANDOM, PN_AUTO_NEARHUE };
+
+class ParameterSet
+{
+protected:
+
+	unsigned long paletteBlendStarttime; // used for blending from one palette into another one
+	unsigned long paletteBlendEndtime;
+	CRGBPalette16 currentPalette;	      // palette in use
+	CRGBPalette16 targetPalette;          // palette being blended into
+	void makeRandom(CRGBPalette16* targetPalette);  // create random palette
+
+public:
+	 // Effect-Parameters
+	 // Effects all have access to a fixed defined group of parameters. 
+	  // Simple effects will not use all of them and some parameters might have to be added here when more effects are being developed
+	 EffectType effectType;		// which effect do we want
+	 uint8_t hue;				// 
+	 uint8_t saturation;		// not implemented yet
+	 uint8_t brightness;
+	 uint8_t vitality;			// not used yet
+	 int8_t speed;				// speed of whatever happens in the effect (e.g. hue rotation)
+	 uint8_t randomness;		// how random should the effect behave. not implemented yet. 
+	 uint8_t hueVariability;	// how fluid should the hue change. not sure yet what it should do. not implemented
+	 uint8_t blurryness;		// to be used to somehow blur2d the effects. not usd yet. 
+
+	 // Channel-Parameters
+	 uint8_t clonecount;		// copies one pattern to more than one position on the target array
+	 uint8_t clonedistance;		// distance between such copies
+	 uint8_t mirroraxes;		// cuts the strip into pieces with mirror effects. not yet implemetned
+	 int8_t	 channelSpeed;		// move the effect around on the full strip (signed value both directions possible)
+	 TimeBase fadeTimeBase;		// shoudl the fading be done in miliseconds or in beats
+	 FadeType fadeType;			// what fading to use (-> channel masks)
+	 long fadeTime;				// how long of short to fade in and out. 0 = no fading
+	 BlendType blendType;		// how is the channel blended onto the already existing pixels. channels are processed starting with 0.
+	 uint16_t numLeds;			// how big is the effect.
+	 uint16_t startPos;			// where is it starting on the target strip
+
+	 // Palette Parameters
+	 TBlendType paletteBlendType;	// LINEAR or None
+	 long paletteBlendDuration;		// how slow should a palette blend into a new one. 
+ public:
+	 ParameterSet();
+	 ~ParameterSet();
+	 struct CRGB getColor(uint8_t index);  // get Color based on Palette 
+	 void setTargetPalette(PaletteName myPalette, long duration = 0);  // blend into a new palette
+
+};
+
+
+
+#endif
+
